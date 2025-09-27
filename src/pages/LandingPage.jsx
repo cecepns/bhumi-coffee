235b9f08cc737cc4421c0c4ec57b2bbf9ee7c912
Cecep Nandang <cecep@kima.one>
2025-09-27 15:23:17 +0700

    fix: path image

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -86,7 +86,7 @@ const LandingPage = () => {
       </section>
 
       {/* Features Section */}
-      {/* <section className="py-20 bg-white">
+      <section className="py-20 bg-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center mb-16" data-aos="fade-up">
             <h2 className="text-3xl md:text-4xl font-bold text-gray-900 mb-4">
@@ -123,7 +123,7 @@ const LandingPage = () => {
             </div>
           </div>
         </div>
-      </section> */}
+      </section>
 
       {/* Menu Section */}
       <section className="py-20 bg-gray-50">
@@ -178,7 +178,7 @@ const LandingPage = () => {
       </section>
 
       {/* Coffee Beans Section */}
-      <section className="py-20 bg-white">
+      {/* <section className="py-20 bg-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center mb-12" data-aos="fade-up">
             <div>
@@ -224,7 +224,7 @@ const LandingPage = () => {
             ))}
           </div>
         </div>
-      </section>
+      </section> */}
 
       <Footer />
     </div>
